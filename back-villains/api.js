3341d21d5c473950977757083e88ec9b1a10b835
Tiffany F <tbluebox@gmail.com>
2018-12-11 11:45:17 +1100

    Updated villains data api

--- back-villains/api.js
+++ back-villains/api.js
@@ -2,3 +2,9 @@ const express = require('express');
 const Joi = require('joi');
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.send('Hey there, looking for a villain? You\'ve come to the right place')
+})
+
