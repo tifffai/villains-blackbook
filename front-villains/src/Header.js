d0f1717f7461bb8160f416640f116917c5862521
Tiffany F <tbluebox@gmail.com>
2018-12-12 11:56:46 +1100

    Added Navbar and Header to fReact files

---
+++ front-villains/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div id="header-box">
+            <img id="disney-logo" src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fcriticsight.com%2Fwp-content%2Fuploads%2F2014%2F12%2Fdisney-logo-blanco-white-2015-criticsight.png&f=1" alt="Disney Logo" width="80"></img>
+            <h1>Villain Blacklist</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ front-villains/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a href="localhost:3000">HOME</a>
+        </nav>
+    )
+}
+
+export default Navbar;
